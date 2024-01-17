8406d526952c13c12f9a1af3320a7bdc8864a1a0
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2024-01-17 10:24:19 +0000

    complete GET/api/articles/article_id/comments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -122,7 +122,6 @@ describe('GET /api/articles/:article_id/comments', () => {
         .get('/api/articles/1/comments')
         .expect(200)
         .then(({body}) => {
-            console.log(body);
             expect(body.comments.length).toBe(11)
             body.comments.forEach((comment) => {
                 expect(comment).toMatchObject({
